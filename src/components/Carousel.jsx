2b90cb9508b13e06445a108cee93aa88912b20fd
mvacjar <mvacjar@gmail.com>
2024-01-29 15:26:48 +0100

    Carousel added

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,148 @@
+const Carousel = () => {
+  return (
+    <div className="m-8 relative overflow-auto">
+      <div className="w-fit">
+        <div className="overflow-x-auto flex">
+          {/* start profile */}
+          <div className="w-56 h-76 m-3 rounded-xl overflow-hidden flex flex-col relative group cursor-pointer">
+            <img
+              className="w-full h-full object-cover transition duration-300 ease-in-out transform group-hover:scale-105"
+              src="https://picsum.photos/200/300?random=1"
+              alt="picture"
+            />
+            <div className="absolute inset-x-3 bottom-1">
+              <p className="text-white font-semibold">Name</p>
+            </div>
+            <div className="absolute inset-0 bg-black opacity-0 transition duration-300 ease-in-out group-hover:opacity-20"></div>
+          </div>
+          {/* end profile */}
+          {/* start profile */}
+          <div className="w-56 h-76 m-3 rounded-xl overflow-hidden flex flex-col relative group cursor-pointer">
+            <img
+              className="w-full h-full object-cover transition duration-300 ease-in-out transform group-hover:scale-105"
+              src="https://picsum.photos/200/300?random=2"
+              alt="picture"
+            />
+            <div className="absolute inset-x-3 bottom-1">
+              <p className="text-white font-semibold">Name</p>
+            </div>
+            <div className="absolute inset-0 bg-black opacity-0 transition duration-300 ease-in-out group-hover:opacity-20"></div>
+          </div>
+          {/* end profile */} {/* start profile */}
+          <div className="w-56 h-76 m-3 rounded-xl overflow-hidden flex flex-col relative group cursor-pointer">
+            <img
+              className="w-full h-full object-cover transition duration-300 ease-in-out transform group-hover:scale-105"
+              src="https://picsum.photos/200/300?random=3"
+              alt="picture"
+            />
+            <div className="absolute inset-x-3 bottom-1">
+              <p className="text-white font-semibold">Name</p>
+            </div>
+            <div className="absolute inset-0 bg-black opacity-0 transition duration-300 ease-in-out group-hover:opacity-20"></div>
+          </div>
+          {/* end profile */} {/* start profile */}
+          <div className="w-56 h-76 m-3 rounded-xl overflow-hidden flex flex-col relative group cursor-pointer">
+            <img
+              className="w-full h-full object-cover transition duration-300 ease-in-out transform group-hover:scale-105"
+              src="https://picsum.photos/200/300?random=4"
+              alt="picture"
+            />
+            <div className="absolute inset-x-3 bottom-1">
+              <p className="text-white font-semibold">Name</p>
+            </div>
+            <div className="absolute inset-0 bg-black opacity-0 transition duration-300 ease-in-out group-hover:opacity-20"></div>
+          </div>
+          {/* end profile */} {/* start profile */}
+          <div className="w-56 h-76 m-3 rounded-xl overflow-hidden flex flex-col relative group cursor-pointer">
+            <img
+              className="w-full h-full object-cover transition duration-300 ease-in-out transform group-hover:scale-105"
+              src="https://picsum.photos/200/300?random=5"
+              alt="picture"
+            />
+            <div className="absolute inset-x-3 bottom-1">
+              <p className="text-white font-semibold">Name</p>
+            </div>
+            <div className="absolute inset-0 bg-black opacity-0 transition duration-300 ease-in-out group-hover:opacity-20"></div>
+          </div>
+          {/* end profile */} {/* start profile */}
+          <div className="w-56 h-76 m-3 rounded-xl overflow-hidden flex flex-col relative group cursor-pointer">
+            <img
+              className="w-full h-full object-cover transition duration-300 ease-in-out transform group-hover:scale-105"
+              src="https://picsum.photos/200/300?random=6"
+              alt="picture"
+            />
+            <div className="absolute inset-x-3 bottom-1">
+              <p className="text-white font-semibold">Name</p>
+            </div>
+            <div className="absolute inset-0 bg-black opacity-0 transition duration-300 ease-in-out group-hover:opacity-20"></div>
+          </div>
+          {/* end profile */} {/* start profile */}
+          <div className="w-56 h-76 m-3 rounded-xl overflow-hidden flex flex-col relative group cursor-pointer">
+            <img
+              className="w-full h-full object-cover transition duration-300 ease-in-out transform group-hover:scale-105"
+              src="https://picsum.photos/200/300?random=7"
+              alt="picture"
+            />
+            <div className="absolute inset-x-3 bottom-1">
+              <p className="text-white font-semibold">Name</p>
+            </div>
+            <div className="absolute inset-0 bg-black opacity-0 transition duration-300 ease-in-out group-hover:opacity-20"></div>
+          </div>
+          {/* end profile */} {/* start profile */}
+          <div className="w-56 h-76 m-3 rounded-xl overflow-hidden flex flex-col relative group cursor-pointer">
+            <img
+              className="w-full h-full object-cover transition duration-300 ease-in-out transform group-hover:scale-105"
+              src="https://picsum.photos/200/300?random=8"
+              alt="picture"
+            />
+            <div className="absolute inset-x-3 bottom-1">
+              <p className="text-white font-semibold">Name</p>
+            </div>
+            <div className="absolute inset-0 bg-black opacity-0 transition duration-300 ease-in-out group-hover:opacity-20"></div>
+          </div>
+          {/* end profile */} {/* start profile */}
+          <div className="w-56 h-76 m-3 rounded-xl overflow-hidden flex flex-col relative group cursor-pointer">
+            <img
+              className="w-full h-full object-cover transition duration-300 ease-in-out transform group-hover:scale-105"
+              src="https://picsum.photos/200/300?random=9"
+              alt="picture"
+            />
+            <div className="absolute inset-x-3 bottom-1">
+              <p className="text-white font-semibold">Name</p>
+            </div>
+            <div className="absolute inset-0 bg-black opacity-0 transition duration-300 ease-in-out group-hover:opacity-20"></div>
+          </div>
+          {/* end profile */}
+          {/* start profile */}
+          <div className="w-56 h-76 m-3 rounded-xl overflow-hidden flex flex-col relative group cursor-pointer">
+            <img
+              className="w-full h-full object-cover transition duration-300 ease-in-out transform group-hover:scale-105"
+              src="https://picsum.photos/200/300?random=10"
+              alt="picture"
+            />
+            <div className="absolute inset-x-3 bottom-1">
+              <p className="text-white font-semibold">Name</p>
+            </div>
+            <div className="absolute inset-0 bg-black opacity-0 transition duration-300 ease-in-out group-hover:opacity-20"></div>
+          </div>
+          {/* end profile */}
+          {/* start profile */}
+          <div className="w-56 h-76 m-3 rounded-xl overflow-hidden flex flex-col relative group cursor-pointer">
+            <img
+              className="w-full h-full object-cover transition duration-300 ease-in-out transform group-hover:scale-105"
+              src="https://picsum.photos/200/300?random=11"
+              alt="picture"
+            />
+            <div className="absolute inset-x-3 bottom-1">
+              <p className="text-white font-semibold">Name</p>
+            </div>
+            <div className="absolute inset-0 bg-black opacity-0 transition duration-300 ease-in-out group-hover:opacity-20"></div>
+          </div>
+          {/* end profile */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import SwipeCTA from "../components/SwipeCTA";
+import Carousel from "../components/Carousel";
 
 export default function MainPage() {
   return (
     <>
       <div className="">
+        <Carousel />
         <SwipeCTA />
       </div>
     </>
